971f151fe6b458720e1dec4bd907e641c1bfdbaf
wending8 <wending8@users.noreply.github.com>
2017-07-22 09:56:58 -0400

    Simplify code

--- software_ISR.c
+++ software_ISR.c
@@ -8,6 +8,7 @@
 	*
 	*	DEVELOPMENT HISTORY:
 	*	2017/06/24		Created.
+	*   2017/07/22      Simplify code.
 	***************************************************************************************
  */
 
@@ -15,21 +16,22 @@
 #include <stdlib.h>
 #include <windows.h>
 
-int flag1, flag2, flag3;
-
 void ISR1(void)
 {
-    flag1 = 1;
+    printf("F1 Key Hit...\n");
+    Sleep(1000);
 }
 
 void ISR2(void)
 {
-    flag2 = 1;
+    printf("F2 Key Hit...\n");
+    Sleep(1000);
 }
 
 void ISR3(void)
 {
-    flag3 = 1;
+    printf("F3 Key Hit...\n");
+    Sleep(1000);
 }
 
 void main()
@@ -37,31 +39,10 @@ void main()
     while(1){
         printf("Processing...\n");
         if (GetAsyncKeyState(VK_F1))
-        {
             ISR1();
-        }
         if (GetAsyncKeyState(VK_F2))
-        {
             ISR2();
-        }
         if (GetAsyncKeyState(VK_F3))
-        {
             ISR3();
-        }
-        if (flag1){
-            flag1 = 0;
-            printf("F1 Key Hit...\n");
-            Sleep(1000);
-        }
-        if (flag2){
-            flag2 = 0;
-            printf("F2 Key Hit...\n");
-            Sleep(1000);
-        }
-        if (flag3){
-            flag3 = 0;
-            printf("F3 Key Hit...\n");
-            Sleep(1000);
-        }
     }
 }
